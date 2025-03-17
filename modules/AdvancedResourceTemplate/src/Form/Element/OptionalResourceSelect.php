1fbabba95725db2ad94d7233a790f6b8aa58682a
Joris Lambrechts <joris.lambrechts@kuleuven.be>
2025-03-17 10:55:46 +0100

    remove font awsome + remove unused modules and themes

--- modules/AdvancedResourceTemplate/src/Form/Element/OptionalResourceSelect.php
+++
@@ -1,8 +0,0 @@
-<?php declare(strict_types=1);
-
-namespace AdvancedResourceTemplate\Form\Element;
-
-class OptionalResourceSelect extends \Omeka\Form\Element\ResourceSelect
-{
-    use TraitOptionalElement;
-}
